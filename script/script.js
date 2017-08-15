1431fe02097e86fe7ca44cc72fbabd8a5381ef9d
Lauren Tremblay <ltremblay4@gmail.com>
2017-08-14 20:47:31 -0400

    js add to page and style

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ var book1 = {
   "author": "Savvy Coders",
   "picture_url": "https://github.com/camilopayan/student-showcase/blob/master/images/class.jpg?raw=true",
   "price": 24,
-  "selling points": [
+  "selling_points": [
     "Awesome coders July 2017",
     "camilo, chloek, ciara.l.nelson, kdub, ltremblay, wang, wrawlings",
     "Bacon ipsum dolor amet flank meatball venison ground round short loin, ball tip prosciutto pancetta.",
@@ -18,7 +18,7 @@ var book2 = {
   "author": "Sasha Ariel Alston",
   "picture_url": "http://www.kickstartchildrensbooks.com/uploads/4/4/5/6/4456781/screen-shot-2017-01-17-at-3-41-24-pm_orig.png",
   "price": 9.99,
-  "selling points": [
+  "selling_points": [
     "Sasha Savvy Loves to Code is here to show little girls that they can program computers for fun and profit. ~ Bustle",
     "Sasha Savvy is here to teach girls that coding the world is a girl thing too! ~ Emory Brown, Beneath the Brand, Talent Zoo",
     "Sasha Savvy Loves to Code is sure to be a huge boost for young girls interested in STEM...~ Blavity",
@@ -31,20 +31,25 @@ var book3 = {
   "author": "Ingrid Law",
   "picture_url": "http://images.penguinrandomhouse.com/cover/9780142414330",
   "price": 6.83,
-  "selling points": [
+  "selling_points": [
     "A Newberry Honor Book",
     "An Oprah's Book Club Kids Rreading List Pick",
     "A New York Times Bestseller",
   ]
 }
-$("#book1 .name").text(book1.name)
-$("#book1 .author").text(book1.author)
-$("#book1 .price").text(book1.price)
-$("#book1 .picture>img").attr(book1.picture_url)
 
 var add_to_page = function(book1) {
-  $('#content').append($("<div id='book" + book1.id + "'>").html( $('<div class=name>').text(book1.name)))
+  $('#main').append($("<div id='book" + book1.id + "'>")
+  .html( $('<div class="name">' + book1.name + '</div>'
+  +'<div class="author">' + "by " + book1.author + '</div>'
+  + '<img class="bookpic" src=' + book1.picture_url + ">"
+  + '<div class="price">' + '$' + book1.price + '</div>'
+  + '<div class="selling">' + book1.selling_points[0] + '</div>')))
 }
+
+add_to_page(book1);
+add_to_page(book2);
+add_to_page(book3);
 
 var count = 2;
 
@@ -60,4 +65,4 @@ var count = 2;
         } );
 
         append_to_page(formObject);
-    });
+  });
